ee4fe2d89e549c4e53629157a1d32aa75e2dee32
mpericles <mpericles1@yahoo.com>
2023-06-21 10:43:00 -0400

    exercise 2 and 3

--- OperatorExercise/Program.cs
+++ OperatorExercise/Program.cs
@@ -7,6 +7,7 @@ namespace OperatorExercise
 
         static void Main(string[] args)
         {
+            // exercise 1 Section
             //Create 2 integer variables
             int a = 17;
             int b = 4;
@@ -48,9 +49,25 @@ namespace OperatorExercise
                     return a;
                 }
             }
+            //exercise 2 Section
+            AreaOfCirlce(30);
+            static double AreaOfCirlce(double radius)
+            {
+                return Math.PI * radius * radius;
+            }
+     
+            
+            //exercise 3 Section - thought exercise
+            // What is the value that is printed out to the console ?:
+            var i = 3;
+            var j = 4;
+            var k = ++i * j++;
+
+
+            // Out of the exercises
             // Output of Exercize#1
             Console.WriteLine(" ");
-            Console.WriteLine("This is The output of the Exercise");
+            Console.WriteLine("This is The output of the Exercise 1");
            
             Console.WriteLine("Addition");
             Console.WriteLine(a + b);
@@ -63,9 +80,21 @@ namespace OperatorExercise
             Console.WriteLine("Modulus Result");
             Console.WriteLine(a % b);
 
-            Console.WriteLine("This one was a confusing one for me");
+            Console.WriteLine("This exercise 1 was a confusing one for me");
             Console.WriteLine("I was trying to use the methods and then i gave up ans also I have 5 warnings and can't figure out why");
-             }
+
+            // Output of Exercize#2
+            Console.WriteLine(" ");
+            Console.WriteLine("This is The output of the Exercise 2");
+            Console.WriteLine(AreaOfCirlce);
+            Console.WriteLine("Why is this not working?");
+
+            // Output of Exercize#3 - Thoought exercise
+            Console.WriteLine(" ");
+            Console.WriteLine("This is The output of the Exercise 3");
+            Console.WriteLine(k);
+
+        }
     }
      
 }
